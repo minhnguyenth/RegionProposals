4098c46efc1b3fb2ebbf685d22c8b83c7b45aed0
minhnguyenxd <nguyenanhminhxd@gmail.com>
2018-03-30 12:01:26 -0700

    initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=regionalproposal
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/regionalproposal
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=regionalproposal.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/regionalproposal.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=regionalproposal
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/regionalproposal
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=regionalproposal.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/regionalproposal.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
